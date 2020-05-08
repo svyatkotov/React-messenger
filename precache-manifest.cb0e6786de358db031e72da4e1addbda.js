eb6c21a75868d151ee3e11f6d4787fcb252e6e69
svyatkotov <skd946@gmail.com>
2020-05-08 04:20:29 +0300

    Updates

---
+++ precache-manifest.cb0e6786de358db031e72da4e1addbda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb012148b0aa25ec0f2f1bb06b6cc73a",
+    "url": "/middle.react.praktikum.yandex/index.html"
+  },
+  {
+    "revision": "ab39d9da2abb6bb93bdc",
+    "url": "/middle.react.praktikum.yandex/static/css/main.089423a9.chunk.css"
+  },
+  {
+    "revision": "7c9162e36c8f95684e68",
+    "url": "/middle.react.praktikum.yandex/static/js/2.53374e15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/middle.react.praktikum.yandex/static/js/2.53374e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab39d9da2abb6bb93bdc",
+    "url": "/middle.react.praktikum.yandex/static/js/main.f8df3986.chunk.js"
+  },
+  {
+    "revision": "c7ce702d4e73cec005f5",
+    "url": "/middle.react.praktikum.yandex/static/js/runtime-main.5876ed40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/middle.react.praktikum.yandex/static/media/icon.5d5d9eef.svg"
+  }
+]);
